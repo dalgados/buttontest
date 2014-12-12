89095a0c410f472d1dc4b6c4cde7cf6c6051817d
Orhan DALGARA <orhan.dalgara@turkticaret.net>
2014-12-12 14:52:05 +0200

    Initial Commit

---
+++ testbutton/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  testbutton
+//
+//  Created by Orhan DALGARA on 12/12/14.
+//  Copyright (c) 2014 TURKTICARETNET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ testbutton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testbutton
+//
+//  Created by Orhan DALGARA on 12/12/14.
+//  Copyright (c) 2014 TURKTICARETNET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
